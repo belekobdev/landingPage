a114d56638c839e2b8a34b1bb5164a24f8099f3d
Azamat <belekovazamat127@gmail.com>
2023-07-16 10:24:06 +0600

    change

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -10,9 +10,12 @@ const Header = () => {
             <div className="container">
                 <div className='flex items-center justify-between'>
                     <img src={logo} alt=""/>
-                    <div className='flex items-center'>
-                        <NavLink className='text-black mx-4 font-bold' to={'/hero'}>Menu</NavLink>
-                        <NavLink className='text-black mx-4 font-bold' to={'/menu'}>Orders </NavLink>
+                    <div className='relative flex items-center'>
+                        <NavLink className='text-black mx-4 font-bold ' to={'/hero'}>Menu</NavLink>
+                       {admin.length === 0 ? null : <p className='absolute left-[50px] top-[-20px] bg-[red] w-[25px] h-[25px] flex items-center justify-center rounded-[50%] text-white'>{admin.length}</p>} 
+                        <NavLink className='text-black mx-[30px] font-bold' to={'/menu'}>Orders </NavLink>
+                       {menu.length === 0 ? null : <p className='absolute left-[150px] top-[-20px] bg-[red] w-[25px] h-[25px] flex items-center justify-center rounded-[50%] text-white'>{menu.length}</p>} 
+
                         <NavLink className='text-black mx-4 font-bold' to={'/'}>Admin </NavLink>
                     </div>
                 </div>
--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -8,8 +8,8 @@ const Menu = () => {
 
 
     return (
+        menu.length === 0 ? <h1 className='bg-black text-white text-center text-[30px] py-5'>NO LIST</h1> :
         <div>
-
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
